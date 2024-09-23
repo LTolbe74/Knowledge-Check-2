d5b5a9629a32af19f6a227454b4bd6824ab5e7df
LTolbe74 <163940324+LTolbe74@users.noreply.github.com>
2024-09-22 20:21:54 -0400

    Add files via upload

---
+++ Knowledge Check 2/Program.cs
@@ -0,0 +1,51 @@
+﻿using System.Drawing;
+using System.Runtime.InteropServices;
+
+namespace Knowledge_Check_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many records do you want to add? ");
+            
+            var numberOfRecords = int.Parse(Console.ReadLine());
+            
+            var recordList = new List<Shoe>();
+            for (int i = 0; i < numberOfRecords; i++)
+            {
+                // In this loop, populate the object's properties using Console.ReadLine()
+                var shoe = new Shoe();
+
+                Console.WriteLine("Enter the name brand of shoe you would like to purchase. ");
+                shoe.Name = Console.ReadLine();
+               
+
+                Console.WriteLine("Enter the color of shoe. ");
+                shoe.Color = Console.ReadLine();
+              
+
+                Console.WriteLine("Enter the price of the shoe. ");
+                shoe.Price = Decimal.Parse(Console.ReadLine());
+               
+
+                Console.WriteLine("Enter the quantity you would like to purchase.");
+                shoe.Quantity =  int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Enter your shoe size.");
+                shoe.Size = int.Parse(Console.ReadLine());
+
+
+                recordList.Add(shoe);
+                
+                
+            }
+            foreach (var item in recordList)
+            {
+                Console.WriteLine(item);
+            }
+            // Print out the list of records using Console.WriteLine()
+
+        }
+    }
+}
---
+++ Knowledge Check 2/Shoe.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Knowledge_Check_2
+{
+    internal class Shoe : AthleticStore
+    {
+        public string Color {  get; set; }
+        public int Size { get; set; }
+
+        
+        public override string ToString()
+        {
+            return "We are processing your order for a size "  + Size + " " + Color + " " + Name + " at a total price of " + Quantity * Price + " dollars." ;
+        }
+
+    }
+   
+}
